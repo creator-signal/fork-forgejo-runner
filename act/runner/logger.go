7209a6be39bb68e042852a7650ab137facbdff34
ChristopherHX <christopher.homberger@web.de>
2025-06-11 20:06:25 +0200

    fix: valueMasker concurrent map write (#5026)
    * valueMasker create secrets copy
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit 7ed767c23a554eefb0a7563c674e7a406393b60f)

--- act/runner/logger.go
+++ act/runner/logger.go
@@ -158,6 +158,10 @@ func withStepLogger(ctx context.Context, stepNumber int, stepID, stepName, stage
 type entryProcessor func(entry *logrus.Entry) *logrus.Entry
 
 func valueMasker(insecureSecrets bool, secrets map[string]string) entryProcessor {
+	ssecrets := []string{}
+	for _, v := range secrets {
+		ssecrets = append(ssecrets, v)
+	}
 	return func(entry *logrus.Entry) *logrus.Entry {
 		if insecureSecrets {
 			return entry
@@ -165,7 +169,7 @@ func valueMasker(insecureSecrets bool, secrets map[string]string) entryProcessor
 
 		masks := Masks(entry.Context)
 
-		for _, v := range secrets {
+		for _, v := range ssecrets {
 			if v != "" {
 				entry.Message = strings.ReplaceAll(entry.Message, v, "***")
 			}
