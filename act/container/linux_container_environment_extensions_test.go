6980cbfda970228d32a9afb4b20506e65ed2d1b0
Felix Kröner <felix.kroener@bruker.com>
2025-11-14 12:00:59 +0100

    refactor: Add comment to explain why `t.Chdir()` is not being used in Windows tests

--- act/container/linux_container_environment_extensions_test.go
+++ act/container/linux_container_environment_extensions_test.go
@@ -36,6 +36,7 @@ func TestContainerPath(t *testing.T) {
 		} {
 			if v.workDir != "" {
 				//nolint:usetesting
+				// 2025-11-14: t.Chdir does not work because it relies on os.Open(".").Chdir() which fails on Windows
 				if err := os.Chdir(v.workDir); err != nil {
 					log.Error(err)
 					t.Fail()
@@ -46,6 +47,7 @@ func TestContainerPath(t *testing.T) {
 		}
 
 		//nolint:usetesting
+		// 2025-11-14: t.Chdir does not work because it relies on os.Open(".").Chdir() which fails on Windows
 		if err := os.Chdir(cwd); err != nil {
 			log.Error(err)
 		}
