4752d6f1b136d4a093f0a47926ce9d00f4ebbae7
Crown0815 <felix.kroener@gmx.de>
2026-03-12 14:09:47 +0100

    refactor: Remove special treatment of non-windows for token access
    See https://code.forgejo.org/forgejo/runner/pulls/1433#issuecomment-81610

--- act/common/git/git.go
+++ act/common/git/git.go
@@ -444,45 +444,9 @@ func git(ctx context.Context, options *gitOptions, args ...string) (string, erro
 			return "", fmt.Errorf("failed to parse remote URL %q to use git token: %w", options.remoteURL, err)
 		}
 
-		// On Windows, Git Credential Manager interferes with credential.helper.
-		// Use http.extraHeader to bypass the credential helper system entirely.
-		if runtime.GOOS == "windows" {
-			auth := "x-access-token:" + options.token
-			authHeader := "Authorization: Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
-			gitArguments = append(gitArguments, "-c", fmt.Sprintf("http.extraHeader=%s", authHeader))
-		} else {
-			credentialsFile, err := os.CreateTemp("", "forgejo-runner-git-token-")
-			if err != nil {
-				return "", fmt.Errorf("failed to create temporary file to store Git token: %w", err)
-			}
-
-			credentialsPath := credentialsFile.Name()
-			defer func() {
-				_ = credentialsFile.Close()
-				if err := os.Remove(credentialsPath); err != nil {
-					log.Warnf("Unable to remove temporary file to store Git token %s: %v", credentialsPath, err)
-				}
-			}()
-
-			// Git credential store expects a URL with embedded credentials.
-			// Use a fixed username to keep behavior consistent across providers.
-			remoteURL.User = url.UserPassword("x-access-token", options.token)
-			if remoteURL.Path == "" {
-				remoteURL.Path = "/"
-			}
-
-			_, err = credentialsFile.Write([]byte(remoteURL.String() + "\n"))
-			if err != nil {
-				return "", fmt.Errorf("failed to write Git token to temporary file %s: %w", credentialsPath, err)
-			}
-			err = credentialsFile.Close()
-			if err != nil {
-				return "", fmt.Errorf("failed to close temporary file %s that stores Git token: %w", credentialsPath, err)
-			}
-
-			gitArguments = append(gitArguments, "-c", fmt.Sprintf("credential.helper=store --file=%s", credentialsPath))
-			gitArguments = append(gitArguments, "-c", "credential.useHttpPath=true")
-		}
+		auth := "x-access-token:" + options.token
+		authHeader := "Authorization: Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+		gitArguments = append(gitArguments, "-c", fmt.Sprintf("http.extraHeader=%s", authHeader))
 	}
 	if options.ignoreInvalidCertificates {
 		gitArguments = append(gitArguments, "-c", "http.sslVerify=false")
