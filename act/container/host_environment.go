eleboucher/runner

refactor: replace backend identity checks with capability queries
Replaces ExecutionsEnvironment.GetLXC() with three capability methods on the interface: BackendName(), SupportsDockerActions(), and ManagesOwnNetworking(). Call sites in act/runner that previously guarded behaviour with rc.IsHostEnv()/GetLXC() now query the capability on the JobContainer directly. This removes the scattered identity checks in favour of a single upfront declaration per backend and avoids the "missed a guard" class of bugs.

Also decouples NewContainerInput from docker/go-connections nat types.
ExposedPorts and PortBindings are now plain map[string]struct{} and map[string][]container.PortBinding; the conversion to nat.PortSet / nat.PortMap stays inside the Docker boundary in docker_run.go.

These are independent improvements to the existing interfaces. They make the runner safer to extend with additional backends without committing to any specific plug-in mechanism.

act/container/host_environment.go
@@ -307,7 +307,7 @@ func (e *HostEnvironment) exec(ctx context.Context, commandparam []string, cmdli
 	command := make([]string, len(commandparam))
 	copy(command, commandparam)
 
-	if e.GetLXC() {
+	if e.LXC {
 		if user == "root" {
 			command = append([]string{"/usr/bin/sudo"}, command...)
 		} else {
@@ -393,7 +393,7 @@ func (e *HostEnvironment) UpdateFromEnv(srcPath string, env *map[string]string) 
 
 func (e *HostEnvironment) Remove() common.Executor {
 	return func(ctx context.Context) error {
-		if e.GetLXC() {
+		if e.LXC {
 			// there may be files owned by root: removal
 			// is the responsibility of the LXC backend
 			return nil
@@ -411,8 +411,19 @@ func (e *HostEnvironment) ToContainerPath(path string) string {
 	return path
 }
 
-func (e *HostEnvironment) GetLXC() bool {
-	return e.LXC
+func (e *HostEnvironment) BackendName() string {
+	if e.LXC {
+		return "lxc"
+	}
+	return "host"
+}
+
+func (*HostEnvironment) SupportsDockerActions() bool {
+	return true
+}
+
+func (*HostEnvironment) ManagesOwnNetworking() bool {
+	return true
 }
 
 func (e *HostEnvironment) GetName() string {

act/container/host_environment_test.go
@@ -20,6 +20,14 @@ import (
 // Type assert HostEnvironment implements ExecutionsEnvironment
 var _ ExecutionsEnvironment = &HostEnvironment{}
 
+func TestHostEnvironment_BackendName(t *testing.T) {
+	e := &HostEnvironment{}
+	assert.Equal(t, "host", e.BackendName())
+
+	eLXC := &HostEnvironment{LXC: true}
+	assert.Equal(t, "lxc", eLXC.BackendName())
+}
+
 func TestCopyDir(t *testing.T) {
 	dir := t.TempDir()
 	ctx := t.Context()

act/container/linux_container_environment_extensions_test.go
@@ -75,3 +75,8 @@ type typeAssertMockContainer struct {
 
 // Type assert Container + LinuxContainerEnvironmentExtensions implements ExecutionsEnvironment
 var _ ExecutionsEnvironment = &typeAssertMockContainer{}
+
+func TestLinuxContainerEnvironmentExtensions_BackendName(t *testing.T) {
+	ext := &LinuxContainerEnvironmentExtensions{}
+	assert.Equal(t, "docker", ext.BackendName())
+}

act/runner/run_context.go
@@ -535,9 +535,21 @@ func (rc *RunContext) prepareJobContainer(ctx context.Context) error {
 		for _, port := range spec.Ports {
 			interpolatedPorts = append(interpolatedPorts, rc.ExprEval.Interpolate(ctx, port))
 		}
-		exposedPorts, portBindings, err := nat.ParsePortSpecs(interpolatedPorts)
+		natExposed, natBindings, err := nat.ParsePortSpecs(interpolatedPorts)
 		if err != nil {
 			return fmt.Errorf("failed to parse service %s ports: %w", serviceID, err)
+		}
+		exposedPorts := make(map[container.Port]struct{}, len(natExposed))
+		for port := range natExposed {
+			exposedPorts[container.Port(port)] = struct{}{}
+		}
+		portBindings := make(map[container.Port][]container.PortBinding, len(natBindings))
+		for port, bindings := range natBindings {
+			pb := make([]container.PortBinding, len(bindings))
+			for i, b := range bindings {
+				pb[i] = container.PortBinding{HostIP: b.HostIP, HostPort: b.HostPort}
+			}
+			portBindings[container.Port(port)] = pb
 		}
 
 		serviceContainerName := createContainerName(rc.jobContainerName(), serviceID)

creator-signal/fork-forgejo-runner

act/container/host_environment.go
@@ -307,7 +307,7 @@ func (e *HostEnvironment) exec(ctx context.Context, commandparam []string, cmdli
 	command := make([]string, len(commandparam))
 	copy(command, commandparam)
 
-	if e.GetLXC() {
+	if e.LXC {
 		if user == "root" {
 			command = append([]string{"/usr/bin/sudo"}, command...)
 		} else {
@@ -393,7 +393,7 @@ func (e *HostEnvironment) UpdateFromEnv(srcPath string, env *map[string]string) 
 
 func (e *HostEnvironment) Remove() common.Executor {
 	return func(ctx context.Context) error {
-		if e.GetLXC() {
+		if e.LXC {
 			// there may be files owned by root: removal
 			// is the responsibility of the LXC backend
 			return nil
@@ -411,8 +411,19 @@ func (e *HostEnvironment) ToContainerPath(path string) string {
 	return path
 }
 
-func (e *HostEnvironment) GetLXC() bool {
-	return e.LXC
+func (e *HostEnvironment) BackendID() string {
+	if e.LXC {
+		return "lxc"
+	}
+	return "host"
+}
+
+func (*HostEnvironment) SupportsDockerContainerActions() bool {
+	return true
+}
+
+func (*HostEnvironment) ManagesOwnNetworking() bool {
+	return true
 }
 
 func (e *HostEnvironment) GetName() string {

act/container/host_environment_test.go
@@ -20,6 +20,14 @@ import (
 // Type assert HostEnvironment implements ExecutionsEnvironment
 var _ ExecutionsEnvironment = &HostEnvironment{}
 
+func TestHostEnvironment_BackendID(t *testing.T) {
+	e := &HostEnvironment{}
+	assert.Equal(t, "host", e.BackendID())
+
+	eLXC := &HostEnvironment{LXC: true}
+	assert.Equal(t, "lxc", eLXC.BackendID())
+}
+
 func TestCopyDir(t *testing.T) {
 	dir := t.TempDir()
 	ctx := t.Context()

act/container/linux_container_environment_extensions_test.go
@@ -75,3 +75,8 @@ type typeAssertMockContainer struct {
 
 // Type assert Container + LinuxContainerEnvironmentExtensions implements ExecutionsEnvironment
 var _ ExecutionsEnvironment = &typeAssertMockContainer{}
+
+func TestLinuxContainerEnvironmentExtensions_BackendID(t *testing.T) {
+	ext := &LinuxContainerEnvironmentExtensions{}
+	assert.Equal(t, "docker", ext.BackendID())
+}

act/runner/run_context.go
@@ -535,9 +535,21 @@ func (rc *RunContext) prepareJobContainer(ctx context.Context) error {
 		for _, port := range spec.Ports {
 			interpolatedPorts = append(interpolatedPorts, rc.ExprEval.Interpolate(ctx, port))
 		}
-		exposedPorts, portBindings, err := nat.ParsePortSpecs(interpolatedPorts)
+		natExposed, natBindings, err := nat.ParsePortSpecs(interpolatedPorts)
 		if err != nil {
 			return fmt.Errorf("failed to parse service %s ports: %w", serviceID, err)
+		}
+		exposedPorts := make(map[container.PortSpec]struct{}, len(natExposed))
+		for port := range natExposed {
+			exposedPorts[container.PortSpec(port)] = struct{}{}
+		}
+		portBindings := make(map[container.PortSpec][]container.PortBinding, len(natBindings))
+		for port, bindings := range natBindings {
+			pb := make([]container.PortBinding, len(bindings))
+			for i, b := range bindings {
+				pb[i] = container.PortBinding{HostIP: b.HostIP, HostPort: b.HostPort}
+			}
+			portBindings[container.PortSpec(port)] = pb
 		}
 
 		serviceContainerName := createContainerName(rc.jobContainerName(), serviceID)