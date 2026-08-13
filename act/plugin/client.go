eleboucher/runner

feat(plugin): execution-environment adapter and reference test plugin
Implements the container.ExecutionsEnvironment interface over the gRPC
client (create/start, exec, copy in/out, env update, health, remove) plus
a standalone reference plugin server. Not yet wired into the runner.

act/plugin/client.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"code.forgejo.org/forgejo/runner/v12/act/container"
 	pluginv1 "code.forgejo.org/forgejo/runner/v12/act/plugin/proto/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -188,6 +189,17 @@ func validateCapabilities(caps *pluginv1.CapabilitiesResponse) error {
 
 func (c *Client) Capabilities() *pluginv1.CapabilitiesResponse {
 	return c.caps
+}
+
+func (c *Client) NewEnvironment(input *container.NewContainerInput, backendOpts map[string]string) container.ExecutionsEnvironment {
+	return &pluginEnvironment{
+		client:      c.rpc,
+		caps:        c.caps,
+		backendOpts: backendOpts,
+		input:       input,
+		stdout:      input.Stdout,
+		stderr:      input.Stderr,
+	}
 }
 
 func (c *Client) Close() error {

act/plugin/testplugin/cmd/main.go
@@ -1,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"code.forgejo.org/forgejo/runner/v12/act/plugin/testplugin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	addr := os.Getenv("PLUGIN_LISTEN_ADDR")
+	if addr == "" {
+		addr = "localhost:0"
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "listen: %v\n", err)
+		os.Exit(1)
+	}
+
+	srv := grpc.NewServer()
+	testplugin.New().Register(srv)
+
+	fmt.Fprintln(os.Stdout, lis.Addr().String())
+
+	if err := srv.Serve(lis); err != nil {
+		fmt.Fprintf(os.Stderr, "serve: %v\n", err)
+		os.Exit(1)
+	}
+}

creator-signal/fork-forgejo-runner

act/plugin/client.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"code.forgejo.org/forgejo/runner/v13/act/container"
 	pluginv1alpha "code.forgejo.org/forgejo/runner/v13/act/plugin/proto/v1alpha"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -204,6 +205,19 @@ func validateCapabilities(caps *pluginv1alpha.CapabilitiesResponse) error {
 
 func (c *Client) Capabilities() *pluginv1alpha.CapabilitiesResponse {
 	return c.caps
+}
+
+func (c *Client) NewEnvironment(input *container.NewContainerInput, backendOpts map[string]string, labelArg string, timeout time.Duration) container.ExecutionsEnvironment {
+	return &pluginEnvironment{
+		client:      c.rpc,
+		caps:        c.caps,
+		backendOpts: backendOpts,
+		labelArg:    labelArg,
+		timeout:     timeout,
+		input:       input,
+		stdout:      input.Stdout,
+		stderr:      input.Stderr,
+	}
 }
 
 func (c *Client) Close() error {

act/plugin/testplugin/cmd/main.go
@@ -1,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"code.forgejo.org/forgejo/runner/v13/act/plugin/testplugin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	addr := os.Getenv("PLUGIN_LISTEN_ADDR")
+	if addr == "" {
+		addr = "localhost:0"
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "listen: %v\n", err)
+		os.Exit(1)
+	}
+
+	srv := grpc.NewServer()
+	testplugin.New().Register(srv)
+
+	fmt.Fprintln(os.Stdout, lis.Addr().String())
+
+	if err := srv.Serve(lis); err != nil {
+		fmt.Fprintf(os.Stderr, "serve: %v\n", err)
+		os.Exit(1)
+	}
+}