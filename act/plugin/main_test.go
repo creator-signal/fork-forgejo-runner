db1a90ce7187f509878972f6f2c17b4630a8ffa9
Mathieu Fenniak <mathieu@fenniak.net>
2026-08-13 13:21:51 -0600

    chore: fix test execution of new packages by linking testutils

---
+++ act/plugin/main_test.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package plugin
+
+import (
+	_ "code.forgejo.org/forgejo/runner/v13/testutils"
+)
